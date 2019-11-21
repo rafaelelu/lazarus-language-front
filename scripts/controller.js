1fed82e613b51fd232b42355a86e7f2d97321992
Rafael Elu <rafaelelu@Rafaels-MacBook-Pro.local>
2019-11-21 15:27:42 -0600

    Save and Load files

--- scripts/controller.js
+++ scripts/controller.js
@@ -5,6 +5,7 @@ let bExecute = document.getElementById("ejecutar");
 let bClear = document.getElementById("limpiar");
 let bSave = document.getElementById("guardar");
 let bLoad = document.getElementById("cargar");
+let hiddenBtn = document.getElementById('real-file');
 
 editor = new Editor(document.getElementById("editor"));
 
@@ -16,18 +17,63 @@ bExecute.addEventListener('click', function () {
 
 bClear.addEventListener('click', function () {
     editor.clear();
-    //terminal.clear();
     canvas.clear();
 });
 
-bSave.addEventListener('click', function () {
+var textFile = null,
+  makeTextFile = function (text) {
+    var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    // returns a URL you can use as a href
+    return textFile;
+  };
+
+  bSave.addEventListener('click', function () {
     pageState = '{ "editor" : ' + editor.toJSON() + ', "canvas" : ' + canvas.toJSON() + ' }';
+    
+    var link = document.createElement('a');
+    link.setAttribute('download', 'codigo.lzl');
+    link.href = makeTextFile(pageState);
+    document.body.appendChild(link);
+
+    // wait for the link to be added to the document
+    window.requestAnimationFrame(function () {
+      var event = new MouseEvent('click');
+      link.dispatchEvent(event);
+      document.body.removeChild(link);
+    });
+    
     terminal.print('Se ha guardado la información\n');
 });
 
-bLoad.addEventListener('click', function () {
-    let jsonString = pageState;
-    let jsonObj = JSON.parse(pageState);
+hiddenBtn.addEventListener('change', function () {
+
+    if (hiddenBtn.value) {
+        file = $('#real-file')[0].files[0];
+    }
+    if(file){
+        var reader = new FileReader()
+        reader.readAsBinaryString(file)
+
+        reader.addEventListener('load', function (e) {
+            res=(e.target.result);
+            loadFile(res);
+          });
+    }
+
+});
+
+function loadFile(value){
+    let jsonString = value;
+    let jsonObj = JSON.parse(value);
     let editorText = jsonObj.editor.text;
     let figures = jsonObj.canvas.figures;
     console.log(jsonObj);
@@ -35,4 +81,8 @@ bLoad.addEventListener('click', function () {
     console.log(figures);
     editor.setText(editorText);
     canvas.setFigures(figures);
+}
+
+bLoad.addEventListener('click', function () {
+    hiddenBtn.click();
 });
